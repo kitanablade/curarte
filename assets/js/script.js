fec7916e83ecc17bb1d4299cacb0cea03d64af79
Akshata Kumbhar <105023659+Akshu3011@users.noreply.github.com>
2022-07-07 14:33:28 -0700

    Added html file structure

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+/*AK- HTML- Materialize framework design */
+$(document).ready(function(){
+  $('.slider').slider();
+});
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+
+
+
+/*KW- Please add your queries for fuctionalities */
